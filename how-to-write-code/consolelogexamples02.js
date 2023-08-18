dfe0893c94f1fd659eb3967e0ed4d9f33c64fcea
Mary Tang <maryhtang@gmail.com>
2023-08-18 08:05:27 -0500

    print a list on different lines

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,6 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+console.log("Food items:", "In no specific order")
+console.log("Banana\nApples\nTurkeys");
