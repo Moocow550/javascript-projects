b74cc4fdc28f616674fba0cc313cd38f21075eb4
Mary Tang <maryhtang@gmail.com>
2023-08-18 08:06:53 -0500

    added forgotten semi-colon

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -2,5 +2,5 @@ console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
 
-console.log("Food items:", "In no specific order")
+console.log("Food items:", "In no specific order");
 console.log("Banana\nApples\nTurkeys");
